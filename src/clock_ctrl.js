4daf3e8d97a6e77ed93ef867302cb9332814ceee
Tolleiv Nietsch <tolleiv.nietsch@aoe.com>
2017-11-28 13:58:56 +0100

    Initial commit ... replicating the clock

---
+++ src/clock_ctrl.js
@@ -0,0 +1,16 @@
+import {PanelCtrl} from 'app/plugins/sdk';
+import moment from 'moment';
+
+export class ClockCtrl extends PanelCtrl {
+  constructor($scope, $injector) {
+    super($scope, $injector);
+    this.updateClock();
+  }
+
+  updateClock() {
+    this.time = moment().format('hh:mm:ss');
+    this.$timeout(() => { this.updateClock(); }, 1000);
+  }
+}
+
+ClockCtrl.templateUrl = 'module.html';
